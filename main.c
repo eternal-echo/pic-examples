e1b47132b406a7ab50c699cec86e0b0e4b0dcf9f
eternal-echo <yuanyu@whut.edu.cn>
2023-08-06 15:19:49 +0800

    [main] 添加串口回调的例程，已验证rx开启和关闭的情况

--- main.c
+++ main.c
@@ -7,9 +7,20 @@
 
 #include "uart/uart.h"
 
+#if (defined(PIC_SERIAL_ENABLED) && defined(PIC_SERIAL_RX_ENABLED))
+void uart_rx_callback(uint8_t* pdata, uint16_t size)
+{
+	uart_transmit(pdata, size);
+}
+#endif // PIC_SERIAL_ENABLED && PIC_SERIAL_RX_ENABLED
+
 void main(void) {
 #ifdef PIC_SERIAL_ENABLED
+#ifdef PIC_SERIAL_RX_ENABLED
+	uart_init(1, uart_rx_callback);
+#else
 	uart_init(1, NULL);
+#endif // PIC_SERIAL_RX_ENABLED
 	uart_printf("example\r\n");
 #endif // PIC_SERIAL_ENABLED
 	while(1)
