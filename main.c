ee6556d5ed9a8935a7e4dc3fb5f3e9fb53f5a58a
eternal-echo <yuanyu@whut.edu.cn>
2023-08-22 17:34:53 +0800

    [main] 精简main函数

--- main.c
+++ main.c
@@ -10,46 +10,12 @@
 #include "gpio/gpio.h"
 #include "adc/adc.h"
 
-#define LED_PIN 2
-
-static uint8_t led_indicate = 0;
-
-/**
- * @brief 定时器回调函数
- * 
- */
-static void timer_callback(void)
-{
-	static uint8_t led_state = 0;
-	static uint16_t cnt = 0;
-	// 1000 0000 us = 1s
-	if(cnt++ == 1000)
-	{
-		cnt = 0;
-		led_state = !led_state;
-		gpio_pin_write(D, LED_PIN, led_state);
-		led_indicate = 1;
-	}
-}
-
 void main(void) {
-	gpio_pin_init(C, 0, GPIO_OUTPUT);
-	gpio_pin_write(C, 0, 1);
-	gpio_pin_init(D, LED_PIN, GPIO_OUTPUT);
-	adc_init(1, NULL); // 初始化ADC模块 AN1
 	uart_init(1, NULL);
-	timer_init(1000, timer_callback);
-	uart_printf("timer and uart example\n");
+	uart_printf("example\n");
 	while(1)
 	{
-		// timer_callback();
-		// __delay_ms(1000);
-
-		 if(led_indicate == 1)
-		 {
-		 	led_indicate = 0;
-		 	uart_printf("%f\n", adc_get_val());
-		 }
+		__delay_ms(1000);
 	}
     return;
 }
