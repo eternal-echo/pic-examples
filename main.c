19e224136a3fad4fa06de74a1e2f3f9a8a5c7d08
eternal-echo <yuanyu@whut.edu.cn>
2023-08-24 14:39:31 +0800

    [main] 读取gpio的例程

--- main.c
+++ main.c
@@ -10,11 +10,19 @@
 #include "gpio/gpio.h"
 #include "adc/adc.h"
 
+#define BUTTON1_PORT GPIO_PORT_B
+#define BUTTON2_PORT GPIO_PORT_B
+#define BUTTON1_PIN GPIO_PIN_2
+#define BUTTON2_PIN GPIO_PIN_3
+
 void main(void) {
 	uart_init(1, NULL);
-	uart_printf("example\n");
+	uart_printf("button example\n");
+	gpio_pin_init(BUTTON1_PORT, BUTTON1_PIN, GPIO_MODE_INPUT);
+	gpio_pin_init(BUTTON2_PORT, BUTTON2_PIN, GPIO_MODE_INPUT);
 	while(1)
 	{
+		uart_printf("button1: %d, button2: %d\n", gpio_pin_read(BUTTON1_PORT, BUTTON1_PIN), gpio_pin_read(BUTTON2_PORT, BUTTON2_PIN));
 		__delay_ms(1000);
 	}
     return;
